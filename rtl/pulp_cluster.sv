8550cfe418493a9f0b4de3a24fc3601016b1ae93
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2023-02-08 13:50:05 +0100

    Binding NB_CHACHE_BANKS to the same value it has in the cluster instantiation.

--- rtl/pulp_cluster.sv
+++ rtl/pulp_cluster.sv
@@ -50,7 +50,7 @@ module pulp_cluster
 
   // I$ parameters
   parameter SET_ASSOCIATIVE         = 4,
-  parameter NB_CACHE_BANKS          = 2,
+  parameter NB_CACHE_BANKS          = 8,
   parameter CACHE_LINE              = 1,
   parameter CACHE_SIZE              = 4096,
   parameter ICACHE_DATA_WIDTH       = 128,
