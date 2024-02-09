9d6f70c0800c087350495f71db81347d05b3a9ab
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-02-09 23:16:59 +0100

    Adapt run scripts to use VSIM.

--- scripts/run_and_exit.tcl
+++ scripts/run_and_exit.tcl
@@ -7,7 +7,11 @@ if {![info exists APP]} {
     set APP "./build/test/test"
 }
 
-vsim +permissive -suppress 3053 -suppress 8885 -lib $VSIM_PATH/work +APP=./build/test/test +notimingchecks +nospecify -t 1ps pulp_cluster_tb_optimized +permissive-off ++./build/test/test
+if {![info exists VSIM]} {
+    set VSIM vsim
+}
+
+$VSIM +permissive -suppress 3053 -suppress 8885 -lib $VSIM_PATH/work +APP=./build/test/test +notimingchecks +nospecify -t 1ps pulp_cluster_tb_optimized +permissive-off ++./build/test/test
 
 add log -r /*
 
