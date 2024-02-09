170083fdd1db1162e364f002c6d51e8262cadaf2
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-02-09 23:28:57 +0100

    Add error suppression for WLF logging.

--- scripts/run_and_exit.tcl
+++ scripts/run_and_exit.tcl
@@ -11,7 +11,7 @@ if {![info exists VSIM]} {
     set VSIM vsim
 }
 
-$VSIM +permissive -suppress 3053 -suppress 8885 -lib $VSIM_PATH/work +APP=./build/test/test +notimingchecks +nospecify -t 1ps pulp_cluster_tb_optimized +permissive-off ++./build/test/test
+$VSIM +permissive -suppress 3053 -suppress 8885 -suppress 12130 -lib $VSIM_PATH/work +APP=./build/test/test +notimingchecks +nospecify -t 1ps pulp_cluster_tb_optimized +permissive-off ++./build/test/test
 
 add log -r /*
 
