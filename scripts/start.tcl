0820203c54ddc09785918242206bd8855d2ed11f
Yvan Tortorella <48508508+yvantor@users.noreply.github.com>
2024-02-10 10:28:07 +0100

    Fix PULP cluster CI. (#44)
    * Add `run_and_exit.tcl`; use specific commits for regressions and runtime.
    
    * Update nonfree flow
    
    * Fix environment sourcing
    
    * Adapt run scripts to use VSIM.
    
    * Add error suppression for WLF logging.
    
    ---------
    
    Co-authored-by: Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
    Co-authored-by: Michael Rogenmoser <michael@rogenmoser.us>

--- scripts/start.tcl
+++ scripts/start.tcl
@@ -3,7 +3,11 @@ if {![info exists VSIM_PATH ]} {
     set VSIM_PATH ""
 }
 
-vsim +permissive -suppress 3053 -suppress 8885 -lib $VSIM_PATH/work +APP=./build/test/test +notimingchecks +nospecify  -t 1ps  pulp_cluster_tb_optimized +permissive-off ++./build/test/test
+if {![info exists VSIM]} {
+    set VSIM vsim
+}
+
+$VSIM +permissive -suppress 3053 -suppress 8885 -suppress 12130 -lib $VSIM_PATH/work +APP=./build/test/test +notimingchecks +nospecify  -t 1ps  pulp_cluster_tb_optimized +permissive-off ++./build/test/test
 
 add log -r /*
 run -all
