8a6cf80d61628cdfd83daf9ebcf34cead8587441
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2024-03-04 15:54:50 +0100

    Make the `hwpe_subsystem` more customizable `HwpeCfg` is added to the `pulp_cluster_package` and a list of HWPEs can be passed to instantiate them. The corresponding selection ID is simply the position inside the list. `MAX_NUM_HWPES` sets an upper limit to the number of HWPEs to comply with the packed nature of the `Cfg` data structure.

--- rtl/hwpe_subsystem.sv
+++ rtl/hwpe_subsystem.sv
@@ -14,36 +14,43 @@
  */
 
 import hci_package::*;
+import pulp_cluster_package::*;
 
 module hwpe_subsystem
 #(
-  parameter int unsigned N_CORES       = 8,
-  parameter int unsigned N_HWPES       = 1,
-  parameter int unsigned N_MASTER_PORT = 9,
-  parameter int unsigned ID_WIDTH      = 8,
-  parameter int unsigned DW            = DEFAULT_DW,
-  parameter int unsigned AW            = DEFAULT_AW,
-  parameter int unsigned OW            = AW
+  parameter  hwpe_subsystem_cfg_t HWPE_CFG = '0,
+  parameter  int unsigned N_CORES          = 8,
+  parameter  int unsigned N_MASTER_PORT    = 9,
+  parameter  int unsigned ID_WIDTH         = 8,
+  parameter  int unsigned DW               = DEFAULT_DW,
+  parameter  int unsigned AW               = DEFAULT_AW,
+  parameter  int unsigned OW               = AW
 )
 (
-  input  logic                       clk,
-  input  logic                       rst_n,
-  input  logic                       test_mode,
-  input  logic                       hwpe_en_i,
-  input  logic [$clog2(N_HWPES)-1:0] hwpe_sel_i,
+  input  logic                             clk,
+  input  logic                             rst_n,
+  input  logic                             test_mode,
+  input  logic                             hwpe_en_i,
+  input  logic [$clog2(MAX_NUM_HWPES)-1:0] hwpe_sel_i,
 
-  hci_core_intf.master               hwpe_xbar_master,
-  XBAR_PERIPH_BUS.Slave              hwpe_cfg_slave,
+  hci_core_intf.master                     hwpe_xbar_master,
+  XBAR_PERIPH_BUS.Slave                    hwpe_cfg_slave,
 
-  output logic [N_CORES-1:0][1:0]    evt_o,
-  output logic                       busy_o
+  output logic [N_CORES-1:0][1:0]          evt_o,
+  output logic                             busy_o
 );
+
+  localparam int unsigned N_HWPES = HWPE_CFG.NumHwpes;
 
   logic [N_HWPES-1:0] busy;
   logic [N_HWPES-1:0][N_CORES-1:0][1:0] evt;
 
   logic [N_HWPES-1:0] hwpe_clk;
   logic [N_HWPES-1:0] hwpe_en_int;
+
+  logic [$clog2(N_HWPES)-1:0] hwpe_sel_int;
+
+  assign hwpe_sel_int = hwpe_sel_i[0+:$clog2(N_HWPES)];
 
   hwpe_ctrl_intf_periph #(
     .ID_WIDTH ( ID_WIDTH )
@@ -55,9 +62,11 @@ module hwpe_subsystem
     .OW ( OW )
   ) tcdm [N_HWPES-1:0] (.clk(clk));
 
-  for (genvar i = 0; i < N_HWPES; i++) begin
+  for (genvar i = 0; i < N_HWPES; i++) begin : gen_hwpe
+
     // HWPE specific enable
-    assign hwpe_en_int[i] = hwpe_en_i && (hwpe_sel_i == i);
+    assign hwpe_en_int[i] = hwpe_en_i && (hwpe_sel_int == i);
+
     // Clock gating cell
     tc_clk_gating i_hwpe_clock_gate (
       .clk_i     ( clk            ),
@@ -65,54 +74,61 @@ module hwpe_subsystem
       .test_en_i ( test_mode      ),
       .clk_o     ( hwpe_clk[i]    )
     );
+
+    // Generate desired HWPEs
+    if (HWPE_CFG.HwpeList[i] == REDMULE) begin : gen_redmule
+
+      /////////////
+      // REDMULE //
+      /////////////
+
+      redmule_top   #(
+        .ID_WIDTH    ( ID_WIDTH         ),
+        .N_CORES     ( N_CORES          ),
+        .DW          ( N_MASTER_PORT*32 )
+      ) i_redmule    (
+        .clk_i       ( hwpe_clk[i] ),
+        .rst_ni      ( rst_n       ),
+        .test_mode_i ( test_mode   ),
+        .busy_o      ( busy[i]     ),
+        .evt_o       ( evt[i]      ),
+        .tcdm        ( tcdm[i]     ),
+        .periph      ( periph[i]   )
+      );
+
+    end else if (HWPE_CFG.HwpeList[i] == NEUREKA) begin : gen_neureka
+
+      /////////////
+      // NEUREKA //
+      /////////////
+
+      neureka_top   #(
+        .PE_H        ( 4        ),
+        .PE_W        ( 4        ),
+        .ID          ( ID_WIDTH ),
+        .N_CORES     ( N_CORES  )
+      ) i_neureka    (
+        // global signals
+        .clk_i       ( hwpe_clk[i] ),
+        .rst_ni      ( rst_n       ),
+        .test_mode_i ( test_mode   ),
+        // events
+        .evt_o       ( evt[i]      ),
+        .busy_o      ( busy[i]     ),
+        // tcdm master ports
+        .tcdm        ( tcdm[i]     ),
+        // periph slave port
+        .periph      ( periph[i]   )
+      );
+
+    end
   end
-
-  /////////////
-  // REDMULE //
-  /////////////
-
-  redmule_top   #(
-    .ID_WIDTH    ( ID_WIDTH         ),
-    .N_CORES     ( N_CORES          ),
-    .DW          ( N_MASTER_PORT*32 )
-  ) i_redmule    (
-    .clk_i       ( hwpe_clk[0]      ),
-    .rst_ni      ( rst_n            ),
-    .test_mode_i ( test_mode        ),
-    .busy_o      ( busy[0]          ),
-    .evt_o       ( evt[0]           ),
-    .tcdm        ( tcdm[0]          ),
-    .periph      ( periph[0]        )
-  );
-
-  /////////////
-  // NEUREKA //
-  /////////////
-
-  neureka_top #(
-    .PE_H    ( 4        ),
-    .PE_W    ( 4        ),
-    .ID      ( ID_WIDTH ),
-    .N_CORES ( N_CORES  )
-  ) i_neureka (
-    // global signals
-    .clk_i       ( hwpe_clk[1] ),
-    .rst_ni      ( rst_n       ),
-    .test_mode_i ( test_mode   ),
-    // events
-    .evt_o       ( evt[1]      ),
-    .busy_o      ( busy[1]     ),
-    // tcdm master ports
-    .tcdm        ( tcdm[1]     ),
-    // periph slave port
-    .periph      ( periph[1]   )
-  );
 
   //////////////////
   // HWPE CFG BUS //
   //////////////////
 
-  // Target signals muxed according to `hwpe_sel_i`
+  // Target signals muxed according to `hwpe_sel_int`
   logic [N_HWPES-1:0]               periph_gnt;
   logic [N_HWPES-1:0][31:0]         periph_r_rdata;
   logic [N_HWPES-1:0]               periph_r_valid;
@@ -120,8 +136,8 @@ module hwpe_subsystem
 
   for (genvar i = 0; i < N_HWPES; i++) begin
     always_comb begin
-      // Initiator signals decoded according to `hwpe_sel_i`
-      periph[i].req  = (hwpe_sel_i == i) ? hwpe_cfg_slave.req : '0;
+      // Initiator signals decoded according to `hwpe_sel_int`
+      periph[i].req  = (hwpe_sel_int == i) ? hwpe_cfg_slave.req : '0;
       // No muxing needed
       periph[i].add  = hwpe_cfg_slave.add;
       periph[i].wen  = hwpe_cfg_slave.wen;
@@ -146,7 +162,7 @@ module hwpe_subsystem
     evt_o  = evt[0];
     busy_o = busy[0];
     for (int i = 1; i < N_HWPES; i++) begin
-      if (hwpe_sel_i == i) begin
+      if (hwpe_sel_int == i) begin
         // Config bus
         hwpe_cfg_slave.gnt     = periph_gnt     [i];
         hwpe_cfg_slave.r_rdata = periph_r_rdata [i];
@@ -176,7 +192,7 @@ module hwpe_subsystem
     .clear_i ( '0              ),
     /*********************/
 
-    .sel_i  ( hwpe_sel_i       ),
+    .sel_i  ( hwpe_sel_int     ),
 
     .in     ( tcdm             ),
     .out    ( hwpe_xbar_master )
