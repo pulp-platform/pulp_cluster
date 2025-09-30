ac9a72c214def4e7c6eedc749b00abdc9175e012
Luigi Ghionda <luigi.ghio.98@gmail.com>
2025-09-30 18:19:39 +0200

    Remove logging of all signals when running in command line mode

--- scripts/run_and_exit.tcl
+++ scripts/run_and_exit.tcl
@@ -13,8 +13,6 @@ if {![info exists VSIM]} {
 
 $VSIM +permissive -suppress 3053 -suppress 8885 -suppress 12130 -suppress 7077 -lib $VSIM_PATH/work +APP=./build/test/test +notimingchecks +nospecify -t 1ps pulp_cluster_tb_optimized +permissive-off ++./build/test/test
 
-add log -r /*
-
 proc run_and_exit {} {
     run -all
     quit -code [examine -radix decimal sim:/pulp_cluster_tb/ret_val(30:0)]
